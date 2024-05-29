1838220b92923b2cb323ad0f7b7ed5956fb70e26
Dzaakyy <dzakynurwahid46@gmail.com>
2024-05-29 09:20:13 +0700

    commit pertama

---
+++ src/helloworld.php
@@ -0,0 +1,10 @@
+<?php
+namespace dzaky\libhallo;
+
+class helloworld{
+    public function sayHello($name){
+        return "hello $name welcome to php and composer to create your own libarary";
+    }
+}
+
+?>
